0b90ee83fcc8e7ad8c8d10d97745e5fe773e1172
s30243 <s30243@pjwstk.edu.pl>
2025-03-17 17:14:26 +0100

    Add GasContainer.cs

---
+++ APBD3/GasContainer.cs
@@ -0,0 +1,21 @@
+﻿namespace APBD3;
+
+public class GasContainer: LiquidContainer, IHazardNotifier
+{
+    public override string Type => "G";
+
+    public float Pressure { get; set; }
+
+    public GasContainer(int tareWeight, int maximumPayload, int height, int depth, bool isHazardous=false) : base(tareWeight, maximumPayload,
+        height, depth, isHazardous)
+    { }
+
+    public override int empty()
+    {
+        int to_empty = (int) (CargoWeight * 0.95);
+        
+        CargoWeight -= to_empty;
+
+        return to_empty;
+    }
+}
