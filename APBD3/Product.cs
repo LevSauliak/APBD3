a8bf6f96bc9094e88765bb9644b78c37943f7a45
s30243 <s30243@pjwstk.edu.pl>
2025-03-17 17:14:36 +0100

    Add Product.cs enum

---
+++ APBD3/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace APBD3;
+
+public enum Product
+{
+    Bananas, 
+    Chocolate,
+    Fish, 
+    Meat,
+    IceCream,
+    FrozenPizza,
+    Cheese,
+    Sausages, 
+    Butter,
+    Eggs
+}
