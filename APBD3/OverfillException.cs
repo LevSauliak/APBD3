408dfc3aba134c4606c3fbdcd6ee0d61d63e983b
Levizor <levizorri@gmail.com>
2025-03-23 13:04:38 +0100

    Simplify OverfillException.cs

--- APBD3/OverfillException.cs
+++ APBD3/OverfillException.cs
@@ -1,19 +1,7 @@
 namespace APBD3;
 public class OverfillException : Exception
 {
-    public uint Input { get; }
-    public uint Filled { get; }
-    public uint Max { get; }
-
-    public OverfillException(uint input, uint filled, uint max) : base()
+    public OverfillException(uint input, uint filled, uint max) : base($"OverfillException: tried to fill {input} kg when already filled {filled} kg and max {max} kg.")
     {
-        this.Input = input;
-        this.Filled = filled;
-        this.Max = max;
-    }
-
-    public override string ToString()
-    {
-        return $"OverfillException: tried to fill {Input} kg when already filled {Filled} kg and max {Max} kg.";
     }
 }
