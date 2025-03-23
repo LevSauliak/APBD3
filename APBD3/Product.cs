fd935d46024cbcf4ad9e7b209517261c58f14640
Levizor <levizorri@gmail.com>
2025-03-23 12:58:13 +0100

    Create Product enum for Refrigerated container

--- APBD3/Product.cs
+++ APBD3/Product.cs
@@ -13,3 +13,23 @@ public enum Product
     Butter,
     Eggs
 }
+
+public static class ProductExtensions
+{
+    public static float Temperature(this Product product)
+    {
+        return product switch
+        {
+            Product.Bananas => 13.3f,
+            Product.Chocolate => 18f,
+            Product.Fish => 2f,
+            Product.Meat => -15,
+            Product.IceCream => -18,
+            Product.FrozenPizza => -30,
+            Product.Cheese => 7.2f,
+            Product.Sausages => 5,
+            Product.Butter => 20.5f,
+            Product.Eggs => 19
+        };
+    }
+}
